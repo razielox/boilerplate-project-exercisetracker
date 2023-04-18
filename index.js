1dfc7c8a719820a64ad726768d8353c06c3b00fd
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-17 22:58:05 -0500

    update4

--- index.js
+++ index.js
@@ -15,19 +15,6 @@ const userSchema = new mongo.Schema({
   count: Number,
   log: [Object]
 })
-
-const exerciseSchema = new mongo.Schema({
-  username: String,
-  description: String,
-  duration: Number,
-  date: String,
-})
-
-const logSchema = new mongo.Schema({
-  username: String,
-  count: Number,
-  log: [Object]
-})
 const userModel = new mongo.model('users', userSchema)
 
 app.get('/', (req, res) => {
@@ -66,39 +53,49 @@ app.get('/api/users/:id/logs', async(request, response) => {
   
   try {
     if (from !== undefined && to !== undefined) {
-      const query = [
+      let query = [
         {$match:{_id: new mongo.Types.ObjectId(userId)}},
           {$project:{
             log:{
               $filter:{
+                limit: Number(limit) || null,
                 input:'$log',
                 as:'item',
                 cond: {$and:[{$gte:['$$item.date', new Date(from)]},{$lte:['$$item.date', new Date(to)]}]}
+                
               }
             }
           }
         }]
-        limit !== undefined ? query.push({$limit: Number(limit)}) : null
-      console.log(query)
+        //limit !== undefined ?  (...query[0].$project.log.$filter, {limit:5}): null
+        console.log(query,limit)
       const getLogsByDate = await userModel.aggregate(query)
       console.log(getLogsByDate)
       return response.json(getLogsByDate)
     } else {
-      console.log('sin fecha')
+
+      const getLogs = await userModel.find({_id:userId},{log:1,_id:1,username:1})
+      //const {_id, username, log} = getLogs[0]
+      
+      //log.map(index => { console.log(index.date, new Date(index.date).getTime())})
+      //const resultLog = {_id, username, count:log.length, log}
+      const finalResponse = {
+         _id: getLogs[0]._id,
+        username: getLogs[0].username,
+        log: getLogs[0].log.map(log => ({
+          description: log.description,
+          duration: log.duration,
+          date: new Date(log.date).toDateString()
+        }))
+         
+      }
+      console.log(getLogs)
+      return response.json(finalResponse)
     }
-    const getLogs = await userModel.find({_id:userId},{log:1,_id:1,username:1})
-    const {_id, username, log} = getLogs[0]
-    
-    log.map(index => {
-      console.log(index.date, new Date(index.date).getTime())
-    })
-    const resultLog = {_id, username, count:log.length, log}
-    response.json(getLogs)
-    //console.log(resultLog)
-  } catch (error) {
-    console.log(error)
-  }
-})
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 app.post('/api/users/:id/exercises', async(request, response) => {
     const {description, duration, date} = request.body
@@ -113,7 +110,18 @@ app.post('/api/users/:id/exercises', async(request, response) => {
     const user = await userModel.find({_id: userId}).exec()
     if(!!user.length) {
       const updateUser = await userModel.findOneAndUpdate({_id: userId},{$push:{log:exerciseFromRequest}},{new:true}).exec()
-      response.json(updateUser)
+      const finalResponse = {
+        _id: updateUser._id,
+        username: updateUser.username,
+        log: updateUser.log.map(log => ({
+          description: log.description,
+          duration: log.duration,
+          date: new Date(log.date).toDateString()
+        }))
+
+      }
+      console.log(finalResponse, updateUser)
+      response.json(finalResponse)
     } else {
       response.send('nada')
     }
